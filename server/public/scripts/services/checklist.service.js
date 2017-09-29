ea0b2f939233182ce498edc7b63d4e0b6f3649e3
Ian Lehfeldt <ian.lehfeldt@gmail.com>
2017-09-29 15:07:03 -0500

    f u l l s t a c k, working into the client from db

--- server/public/scripts/services/checklist.service.js
+++ server/public/scripts/services/checklist.service.js
@@ -1,3 +1,9 @@
 myApp.service('ChecklistService', ['$http', function($http){
+    self.carsstatus = { list: [] };
 
+    self.getChecklistsStatus = () => {
+        $http.get('/checklist').then(function (response) {
+            self.carsstatus.list=response.data;
+        })
+    }
 }]);
--- server/routes/checklist.router.js
+++ server/routes/checklist.router.js
@@ -1,8 +1,29 @@
 var express = require('express');
 var router = express.Router();
 
+//Checklists status page
+router.get('/', function(req, res) {
+    pool.connect(function (err, client, done) {
+        if (err) {
+            console.log('Error connecting to database', err);
+            res.sendStatus(500);
+        } else {
+            client.query('SELECT customer_info.name, vehicle_info.year, vehicle_info.make, vehicle_info.model, vehicle_info.vin, vehicle_info.service_requested, cars_checklist.checklist_status FROM cars_checklist JOIN vehicle_info ON vehicle_info.vehicle_id=cars_checklist.vehicle_id JOIN customer_info ON customer_info.customer_id=vehicle_info.customer_id WHERE NOT cars_checklist.checklist_status=finished;', 
+            function(err, result) {
+                if (err) {
+                    console.log('Error selecting checklists', err);
+                    res.sendStatus(500);
+                } else {
+                    res.send(result.rows);
+                }
+            })
+        }
+    })
+});
+
 // Initial Checklist population
 // Assuming that this is the first time mechanic has opened the checklist, checklist will populate with necessary information and is setting up for updates.
+//This could also work with checklist histories(??)
 //Basic checklist form
 router.get('/:customer_id/:status', function(req, res) {
     pool.connect(function (err, client, done) {
@@ -23,25 +44,5 @@ router.get('/:customer_id/:status', function(req, res) {
         }
     })
 });
-
-//Checklist status page
-router.get('/checklist_status', function(req, res) {
-    pool.connect(function (err, client, done) {
-        if (err) {
-            console.log('Error connecting to database', err);
-            res.sendStatus(500);
-        } else {
-            client.query('SELECT customer_info.name, vehicle_info.year, vehicle_info.make, vehicle_info.model, vehicle_info.vin, vehicle_info.service_requested, cars_checklist.checklist_status FROM cars_checklist JOIN vehicle_info ON vehicle_info.vehicle_id=cars_checklist.vehicle_id JOIN customer_info ON customer_info.customer_id=vehicle_info.customer_id;', 
-            function(err, result) {
-                if (err) {
-                    console.log('Error selecting checklists', err);
-                    res.sendStatus(500);
-                } else {
-                    res.send(result.rows);
-                }
-            })
-        }
-    })
-})
 
 module.exports = router;
