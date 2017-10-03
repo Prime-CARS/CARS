c158332ee811e31e1260317a2cc8bf88cc045497
Alex Jung <alexandermjung89@gmail.com>
2017-10-03 10:27:52 -0500

    added pending column functions

--- server/public/scripts/controllers/admin.controller.js
+++ server/public/scripts/controllers/admin.controller.js
@@ -9,7 +9,7 @@ myApp.controller('AdminController', function(AdminService) {
   vm.nrqstneeded = false;
   vm.showAuths = false;
   vm.showSearchs = false;
-
+  vm.Prints = AdminService.Prints;
   vm.RetrievedRequests = AdminService.Requests;
 
 
@@ -51,7 +51,6 @@ myApp.controller('AdminController', function(AdminService) {
     vm.nrqstneeded = false;
     vm.getRequests();
   }
-
   vm.search_history = function(z){
     console.log('search button hit, passed', z, 'as search stuff')
   }
@@ -67,7 +66,16 @@ myApp.controller('AdminController', function(AdminService) {
     vm.showAuths = false;
   }
 
-  vm.getRequests()
+  vm.getPrint = function () {
+    AdminService.getPrints()
+  }
+
+  vm.history_view= function(){
+    alert('You hit a button......good job......')
+  }
+
+  vm.getPrint();
+  vm.getRequests();
   console.log(vm.RetrievedRequests)
 
 });
