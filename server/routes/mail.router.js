22d4502576e72fccf68621ba4d7ceefca081329f
Julie Zimmer <julie.berthiaume@gmail.comgit config --global user.email julie.berthiaume@gmail.com>
2017-10-03 17:40:03 -0500

    nodemailer appears to be working

---
+++ server/routes/mail.router.js
@@ -0,0 +1,33 @@
+var express = require('express');
+var router = express.Router();
+//this brings in the nodemailer module
+var nodemailer = require('nodemailer');
+
+
+router.post('/', function (req, res, next) {//this contacts the modemailer service through the server
+    var transporter = nodemailer.createTransport({
+        service: 'Gmail', 
+        auth: {
+            user: 'newrequestcarsministrysystem@gmail.com',
+            pass: 'RequestForService'
+        }
+    });
+
+    var mailOptions = {
+        from: 'newrequestcarsministrysystem@gmail.com',
+        to: 'julie.berthiaume@gmail.com', /* this will be Mitch's email account, whatever he chooses to use to receive theses notifications */
+        subject: 'New request for service form',
+        text: 'A new Request for Services form has just been created and submitted.' 
+    };
+
+    transporter.sendMail (mailOptions, function(error, info){
+        if(error){
+            console.log("There was an error sending the email: ", error);
+        } else {
+            console.log("Email sent: " + info.response);
+            res.sendStatus(201);
+        }
+    });
+}); //end of router.post 
+
+module.exports = router;
