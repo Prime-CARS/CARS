d0042dccd57b61e878a5519ab5429c92257b3c04
Eric Austin <ewaustin@gmail.com>
2017-10-05 23:25:32 -0500

    added placeholder

--- server/public/scripts/controllers/admin.controller.js
+++ server/public/scripts/controllers/admin.controller.js
@@ -1,6 +1,6 @@
 myApp.controller('AdminController', function (AdminService, RequestService, AuthService, $location, $http) {
 
-  $('.maskPhone').mask('(000) 000-0000')
+  $('.maskPhone').mask('(000) 000-0000', {placeholder: "(     )      -     "})
   
   console.log('AdminController created');
   var vm = this;
