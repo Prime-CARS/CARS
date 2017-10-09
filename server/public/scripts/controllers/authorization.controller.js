7ef112d03edf28841152ed8495d7ce6770de7b32
Eric Austin <ewaustin@gmail.com>
2017-10-09 15:38:30 -0500

    still in progress

--- server/public/scripts/controllers/authorization.controller.js
+++ server/public/scripts/controllers/authorization.controller.js
@@ -1,21 +1,30 @@
-myApp.controller('AuthController', function (AuthService, RequestService) {
-   console.log('AuthController created');
+myApp.controller('AuthController', function (AuthService, RequestService, $scope) {
+    console.log('AuthController created');
 
-   var self = this;
-   self.AuthService = AuthService;
-   self.serviceObject = AuthService.serviceObject;
-   self.date = new Date();
+    var self = this;
+    self.AuthService = AuthService;
+    self.serviceObject = AuthService.serviceObject;
+    scope = $rootScope;    
+    scope.serviceObject = AuthService.serviceObject;
+    self.date = new Date();
 
-   AuthService.getAuth()
+    AuthService.getAuth()
 
-   self.updateCustomer = function (z,x){
-       RequestService.updateCustomer(z,x)
-   }
-      
-   self.printDiv = function() {
-      window.print();
-   }
+    self.updateCustomer = function (z, x) {
+        RequestService.updateCustomer(z, x)
+    }
+
+    self.printDiv = function () {
+        window.print();
+    }
+
+    self.logMe = function () {
+        console.log('logMe!');
+    }
+
+    scope.$watch('serviceObject', self.logMe, true)
+
 
 });
-   
 
+// AuthService.saveObject(self.serviceObject)
--- server/routes/authorization.router.js
+++ server/routes/authorization.router.js
@@ -31,4 +31,27 @@ router.get('/', function (req, res) {
     }
 });
 
+// router.put('/', function (req, res) {
+
+//     //connecting to db
+
+//     pool.connect(function (errorConnectingToDatabase, client, done) {
+//         //checking the status of the connection
+//         if (errorConnectingToDatabase) { //if the connection failed
+//             console.log("error connecting to customer_info table in db: ", errorConnectingToDatabase);
+//             res.sendStatus(500);
+//         } else { //the connection is successful
+//             client.query('UPDATE customer_info SET service_status = $1 WHERE customer_id = $2;', [service_status, index], function (errorMakingQuery, result) {
+//                 done;
+//                 if (errorMakingQuery) {
+//                     console.log("Error making db query in customer_info table in requestService.router.js: ", errorMakingQuery);
+//                     res.sendStatus(500);
+//                 } else {
+//                     res.send('Update Service Put Route OK', service_status, index);
+//                 }
+//             });
+//         }
+//     });
+// });
+
 module.exports = router;
