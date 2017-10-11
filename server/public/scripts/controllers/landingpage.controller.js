3c0e47ef0d21bbf6be1b6dee3432b45033bb6166
Alex Jung <alexandermjung89@gmail.com>
2017-10-11 14:46:43 -0500

    some updates

--- server/public/scripts/controllers/landingpage.controller.js
+++ server/public/scripts/controllers/landingpage.controller.js
@@ -4,9 +4,29 @@ myApp.controller('LandingPageController', function(AdminService, $location) {
   var vm = this;
   vm.AdminService = AdminService;
   
-    vm.currentNavItem = 'page1';
+    vm.who= false;
+    vm.whoDo= false;
 
-    vm.goto = function (page) {
+
+
+
+
+    vm.whoExpand= function(){
+      vm.who = true;
+    }
+    vm.whoExpandShrink= function(){
+      vm.who = false;
+    }
+
+    vm.whoExpandDo = function () {
+      vm.whoDo = true;
+    }
+    vm.whoExpandShrinkDo = function () {
+      vm.whoDo= false;
+    }
+    
+    vm.currentNavItem = 'page1';
+      vm.goto = function (page) {
       console.log("Goto " + "'" + page + "'");
       $location.path("'" + page + "'")
     }
