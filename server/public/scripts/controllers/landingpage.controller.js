00f937296fb6e1c11c1dfe343bec8d6516873914
Alex Jung <alexandermjung89@gmail.com>
2017-10-11 16:00:45 -0500

    front page formating updated

--- server/public/scripts/controllers/landingpage.controller.js
+++ server/public/scripts/controllers/landingpage.controller.js
@@ -6,9 +6,9 @@ myApp.controller('LandingPageController', function(AdminService, $location) {
   
     vm.who= false;
     vm.whoDo= false;
-
-
-
+    vm.how = false;
+    vm.when = false;
+    vm.what = false;
 
 
     vm.whoExpand= function(){
@@ -21,10 +21,29 @@ myApp.controller('LandingPageController', function(AdminService, $location) {
     vm.whoExpandDo = function () {
       vm.whoDo = true;
     }
-    vm.whoExpandShrinkDo = function () {
+    vm.whoExpandDoShrink = function () {
       vm.whoDo= false;
     }
-    
+    vm.howExpand = function () {
+      vm.how = true;
+    }
+    vm.howExpandShrink = function () {
+      vm.how = false;
+    }
+    vm.whenExpand = function () {
+      vm.when = true;
+    }
+    vm.whenExpandShrink = function () {
+      vm.when = false;
+    }
+    vm.whatExpand = function () {
+      vm.what = true;
+    }
+    vm.whatExpandShrink = function () {
+      vm.what = false;
+    }
+
+
     vm.currentNavItem = 'page1';
       vm.goto = function (page) {
       console.log("Goto " + "'" + page + "'");
